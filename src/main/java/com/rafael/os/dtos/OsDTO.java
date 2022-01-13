6fefca431581059cd5064a7e0258e9718229c7e1
= <=>
2022-01-13 15:51:07 -0300

    ajustes na API

--- src/main/java/com/rafael/os/dtos/OsDTO.java
+++ src/main/java/com/rafael/os/dtos/OsDTO.java
@@ -7,6 +7,8 @@ import javax.validation.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rafael.os.domain.OS;
+import com.rafael.os.domain.enums.Prioridade;
+import com.rafael.os.domain.enums.Status;
 
 public class OsDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
@@ -62,8 +64,8 @@ public class OsDTO implements Serializable {
 		this.dataFechamento = dataFechamento;
 	}
 
-	public Integer getPrioridade() {
-		return prioridade;
+	public Prioridade getPrioridade() {
+		return Prioridade.toEnum(this.prioridade);
 	}
 
 	public void setPrioridade(Integer prioridade) {
@@ -78,8 +80,8 @@ public class OsDTO implements Serializable {
 		this.observacoes = observacoes;
 	}
 
-	public Integer getStatus() {
-		return status;
+	public Status getStatus() {
+		return Status.toEnum(this.status);
 	}
 
 	public void setStatus(Integer status) {
